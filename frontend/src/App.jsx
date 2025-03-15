ebe52bef78133502c5f1d82b30e270615dc419ab
EIiias <121342511+EIiias@users.noreply.github.com>
2025-03-15 17:12:00 +0100

    Added 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Login from "./login/login";
 import Dashboard from "./dashboard/dashboard"; 
 import TasksPage from "./tasksPage/tasksPage"; 
+import NotFound from "./404/404"; // Import the 404 component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tasks" element={<TasksPage />} />
+        
+        {/* Fallback route for undefined paths */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
